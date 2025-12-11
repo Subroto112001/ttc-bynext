025530e972582326804665d27387cd1ec80f05f3
Subroto112001 <subrotokumarbarman@gmail.com>
2025-12-11 15:07:00 +0600

    notice board added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
-
+import { IoMdNotifications } from "react-icons/io";
+import noticeimage from "../assets/bg_notice_board.png";
+import Link from "next/link";
 export default function Home() {
+  const noticeList = [
+    {
+      id: 1,
+      title: "নতুন কোর্স শুরু সম্পর্কে বিজ্ঞপ্তি",
+      date: "2024-09-01",
+    },
+    {
+      id: 2,
+      title: "ছুটির দিন ঘোষণা",
+      date: "2024-08-25",
+    },
+    {
+      id: 3,
+      title: "নিবন্ধনের শেষ তারিখ প্রসারিত",
+      date: "2024-08-20",
+    },
+    {
+      id: 4,
+      title: "কারিগরি প্রশিক্ষণ কেন্দ্রের কার্যক্রম শুরু",
+      date: "2024-08-15",
+    },
+    {
+      id: 5,
+      title: "নতুন প্রশিক্ষক নিয়োগ বিজ্ঞপ্তি",
+      date: "2024-08-10",
+    },
+    {
+      id: 6,
+      title: "প্রশিক্ষণার্থীদের জন্য নির্দেশিকা প্রকাশ",
+      date: "2024-08-05",
+    },
+  ];
   return (
-    <div className="container">
-      <div className="relative">
+    <div className="container bg-white">
+      {/* banner section */}
+      <section className="relative">
         <picture>
           <img
             src="https://ttcpirganj.com/wp-content/uploads/2024/09/WhatsApp-Image-2024-09-02-at-22.31.43_498ace3a.jpg"
@@ -11,10 +46,41 @@ export default function Home() {
             className="w-full h-[200px] object-cover"
           />
         </picture>
-        <div className="absolute top-[20px] left-3 text-5xl font-bold text-white shadow-2xl simple-shadow">
-          <h1>কারিগরি প্রশিক্ষণ কেন্দ্র, পীরগঞ্জ</h1>
+        <div className="absolute top-[20px] flex flex-row md:flex-col gap-4 left-3 ">
+          <picture>
+            <img
+              src="https://ttcpirganj.com/wp-content/uploads/2024/08/TTC-Pirganj-Logo-768x768.png"
+              alt="ttc-pirganj-logo"
+              className="w-20 h-20 object-contain"
+            />
+          </picture>
+          <h1 className="text-2xl md:text-4xl font-bold text-white simple-shadow">
+            কারিগরি প্রশিক্ষণ কেন্দ্র, পীরগঞ্জ
+          </h1>
         </div>
-      </div>
+      </section>
+      {/* notice section */}
+      <section className=" md:mt-[30px] py-2 md:py-4">
+        <div className="relative flex items-center gap-4 pl-[20px] md:pl-[100px] p-3 w-full md:max-w-2/3 bg-gray-200 border border-gray-400">
+          <div className="left-0 top-0 absolute">
+            <Image src={noticeimage} alt="notice" className=" w-20 h-20 md:w-40 md:h-40" />
+          </div>
+          <div className="flex flex-col z-10 ml-[20px] mt-20 md:mt-0 md:ml-[80px] relative">
+            <h3 className="text-4xl">নোটিশ বোর্ড</h3>
+            <ul className="flex flex-col gap-2 py-[20px] pb-10 md:pb-0">
+              {noticeList.map((notice) => (
+                <li key={notice.id} className="flex items-center gap-2">
+                  <Link href="#" className="flex items-center gap-2">
+                    <IoMdNotifications className="text-xl text-[#72AB20]" />
+                    <span className="font-medium">{notice.title}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <span className="absolute right-2.5 bottom-2.5"><Link href="#">সকল নোটিশ দেখুন</Link></span>
+        </div>
+      </section>
     </div>
   );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,51 +1,76 @@
 "use client";
 
-import React, { useState } from 'react'
+import Link from "next/link";
+import React, { useState } from "react";
 import { IoIosShareAlt } from "react-icons/io";
+import { TiHome } from "react-icons/ti";
+
 const Header = () => {
-    const navitem = [
-      { name: "আমাদের সম্পর্কে", link: "#", color: "#ff6600" },
-      { name: "কোর্স সমূহ", link: "#", color: "#c40a2a" },
-      { name: "নিবন্ধন", link: "#", color: "#84154d" },
-      { name: "ব্যবস্থাপনা", link: "#", color: "#098346" },
-      { name: "গ্যালারি", link: "#", color: "#1399be" },
-      { name: "যোগাযোগ", link: "#", color: "#8768de" },
-      { name: "নোটিশ", link: "#", color: "#555555" },
-    ];
-    const [hoveredcolor, setHoveredcolor] = useState(null);
-    const handleMouseOver = (color) => {
-      setHoveredcolor(color);
-    }
+  const navitem = [
+    { name: "আমাদের সম্পর্কে", link: "#", color: "#ff6600" },
+    { name: "কোর্স সমূহ", link: "#", color: "#c40a2a" },
+    { name: "নিবন্ধন", link: "#", color: "#84154d" },
+    { name: "ব্যবস্থাপনা", link: "#", color: "#098346" },
+    { name: "গ্যালারি", link: "#", color: "#1399be" },
+    { name: "যোগাযোগ", link: "#", color: "#8768de" },
+    { name: "নোটিশ", link: "#", color: "#555555" },
+  ];
+
+  const [hoveredcolor, setHoveredcolor] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <div className="flex flex-col container">
-      <div className="bg-[var(--header-color)] p-2 w-full  flex justify-between items-center mx-auto">
+      <div className="bg-[var(--header-color)] p-2 w-full flex justify-between items-center mx-auto">
         <p className="text-white font-medium text-lg">
           কারিগরি প্রশিক্ষণ কেন্দ্র, পীরগঞ্জ
         </p>
-        <button className="flex justify-center  items-center p-1 bg-white rounded gap-2">
-          শেয়ার{" "}
+
+        <button className="flex justify-center items-center p-1 bg-white rounded gap-2">
+          শেয়ার{" "}
           <span>
             <IoIosShareAlt />
           </span>
         </button>
       </div>
-      <div className="bg-white p-2 text-sm flex  gap-4 border-b border-gray-300">
+
+      <div className="bg-white p-2 text-sm flex gap-4 border-b border-gray-300">
         সরকারি অন্যান্য পোর্টাল দেখুন
       </div>
-      <div className="bg-white ">
+
+      <ul className="bg-white flex flex-col md:flex-row md:items-center">
+        <span className="text-2xl font-medium hover:text-white hover:bg-black px-2 py-1">
+          <TiHome />
+        </span>
         {navitem.map((item, index) => (
-          <a
-            key={index}
-            href={item.link}
-            className={`text-sm font-medium text-[${item.color}] hover:text-white hover:bg-[${item.color}] px-3 py-2 `}
-                onMouseOver={() => handleMouseOver(item.color)}
-                onMouseLeave={()=> setHoveredcolor(null)}
-          >
-            {item.name}
-          </a>
+          <li key={index}>
+            <Link
+              href={item.link}
+              className="text-sm font-medium px-3 py-2 transition-colors duration-200"
+              style={{
+                color: hoveredIndex === index ? "white" : item.color,
+                backgroundColor:
+                  hoveredIndex === index ? item.color : "transparent",
+              }}
+              onMouseOver={() => {
+                setHoveredcolor(item.color);
+                setHoveredIndex(index);
+              }}
+              onMouseLeave={() => {
+                setHoveredcolor(null);
+                setHoveredIndex(null);
+              }}
+            >
+              {item.name}
+            </Link>
+          </li>
         ))}
-        <div className="h-3 w-full bg-white" style={{ backgroundColor: hoveredcolor }}></div>
-      </div>
+      </ul>
+
+      <div
+        className="h-3 w-full hidden md:block transition-colors duration-200"
+        style={{ backgroundColor: hoveredcolor || "white" }}
+      ></div>
     </div>
   );
 };
