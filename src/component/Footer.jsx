419c130a32ae359ad6132fa80cf7287a84828a05
Subroto112001 <subrotokumarbarman@gmail.com>
2025-12-12 14:46:34 +0600

    footer bug update

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -12,7 +12,10 @@ const Footer = () => {
         <div className="bg-[#EBEBEB] flex justify-between items-center py-2 px-2">
           <p className="text-[12px]">সাইটটি শেষ হাল-নাগাদ করা হয়েছে: ২০২৫</p>
           <p className="text-[12px] text-center">
-            পরিকল্পনা ও বাস্তবায়নে :কারিগরি প্রশিক্ষণ কেন্দ্র, পীরগঞ্জ{" "}| <Link href="#" className='hover:text-gray-700'>Subroto Kumar Bamrman</Link>{""} | <Link href="#">ranocoder.ltd</Link>
+            পরিকল্পনা ও বাস্তবায়নে : <Link href="#">ranocoder Ltd.</Link> &nbsp;|&nbsp;
+            <Link href="#" className="hover:text-gray-700">
+              Subroto Kumar Bamrman
+            </Link>
           </p>
         </div>
       </div>
